13bb9436655f2e4eaaad369a32895727d9fb39a4
Lorenzo Marcelino <lorenzommarcelino@gmail.com>
2022-11-09 19:19:56 -0300

    Update and rename 1-Dados_com_muitas_dimensões to 1-Dados_com_muitas_dimensões.py

---
+++ Lidando_com_dados_de_muitas_dimensões/1-Dados_com_muitas_dimensões.py
@@ -0,0 +1,25 @@
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from numpy import random
+import pandas as pd
+
+resultados_exames = pd.read_csv("/content/exames.csv")
+resultados_exames.head()
+
+SEED = 123143
+random.seed(SEED)
+
+valores_exames = resultados_exames.drop(columns=['id', 'diagnostico'])
+diagnostico = resultados_exames.diagnostico
+valores_exames_v1 = valores_exames.drop(columns="exame_33")
+
+treino_x, teste_x, treino_y, teste_y = train_test_split(valores_exames_v1, 
+                                                        diagnostico,
+                                                        test_size = 0.3)
+
+
+
+classificador = RandomForestClassifier(n_estimators = 100)
+classificador.fit(treino_x, treino_y)
+print("Resultado da classificação %.2f%%" %(classificador.score(teste_x,teste_y)*100))
